31866f75b22bbd8b6a9f6d44c5379409d7598f3d
tanji <nicholastanjerome@gmail.com>
2013-11-11 23:45:42 +0100

    interactive demo working.
    Signed-off-by: tanji <nicholastanjerome@gmail.com>

--- js/jquery.tancolor.js
+++ js/jquery.tancolor.js
@@ -1,30 +1,22 @@
 (function ($) {  
     $.fn.tancolor = function(options) {
         var settings = $.extend({
-            mode: '',
             r_weight: 0.34,
             g_weight: 0.5,
             b_weight: 0.16,
             r_intensity: 1,
             g_intensity: 1,
             b_intensity: 1
-        }, options);
+        }, options );
 
-        console.log('tancolor.js');
-        console.log(settings.r_weight);
-        console.log(settings.g_weight);
-        console.log(settings.b_weight);
-        console.log(settings.r_intensity);
-        console.log(settings.g_intensity);
-        console.log(settings.b_intensity);
-        
         var r_weight;
         var g_weight;
         var b_weight;
         var r_intensity;
         var g_intensity;
         var b_intensity;
-
+        
+        // settings value
         switch(settings.mode){
             case 'grayscale':
                 r_weight = 0.34;
@@ -67,25 +59,22 @@
                 b_intensity = settings.b_intensity;
                 break;
         }
-        
-        console.log('final tancolor.js');
+
         console.log(r_weight);
         console.log(g_weight);
         console.log(b_weight);
         console.log(r_intensity);
         console.log(g_intensity);
         console.log(b_intensity);
-        
-        canvas = document.createElement("canvas");
-        canvas.width = $(this).width();
-        canvas.height = $(this).height();
+
+        // convert image to canvas
         var img = document.getElementById($(this).attr("id"));
-        canvas.getContext("2d").drawImage(img, 0, 0);
-        var context = canvas.getContext('2d');
-        context.drawImage(img, 0 , 0);
+        var canvas = convertImageToCanvas(img);
+        var ctx = canvas.getContext("2d");
+        var imageData = ctx.getImageData(0, 0, img.width, img.height)
         $(this).replaceWith(canvas);
 
-        var imageData = context.getImageData(0, 0, img.width, img.height);
+        // Processing image data
         var data = imageData.data;
         for(var i = 0; i < data.length; i += 4) {
             var brightness = r_weight * data[i] + g_weight * data[i + 1] + b_weight * data[i + 2];
@@ -96,6 +85,34 @@
             // blue
             data[i + 2] = b_intensity * brightness;
         }
-        context.putImageData(imageData, 0, 0);
+        ctx.putImageData(imageData, 0, 0);
+
+        $('#'+$(this).attr("id")).each(function(i,e){ 
+             var img = e.toDataURL("image/png"); 
+             $(e).replaceWith( $('<img src="' + img + '"/>').attr({width: $(e).attr("width"), height: $(e).attr("height"), style: $(e).attr("style"), id: $(e).attr("id") }) ) });
+
+        // Converts image to canvas; returns new canvas element
+        function convertImageToCanvas(image) {
+	    var canvas = document.createElement("canvas");
+            image.src = "images/obama.jpg";
+	    canvas.width = image.width;
+	    canvas.height = image.height;
+            if(image.id) {
+                canvas.id = image.id;
+            }
+            if(image.className) {
+                canvas.className = image.className;
+            }
+	    canvas.getContext("2d").drawImage(image, 0, 0);
+
+	    return canvas;
+        }
+
+        // Converts canvas to an image
+        function convertCanvasToImage(canvas) {
+	    var image = new Image();
+	    image.src = canvas.toDataURL("image/png");
+	    return image;
+        }
     };
 }(jQuery));
